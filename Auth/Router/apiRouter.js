74b42656b919ab0df654d3f571544cff0d11d8e6
Majo <mariajose72003@gmail.com>
2024-10-11 01:36:16 -0500

    Add configuration for swagger doc

--- Auth/Router/apiRouter.js
+++ Auth/Router/apiRouter.js
@@ -8,7 +8,40 @@ const { validateCreateUser} = require('../Validators/registerValidators');
 const { validateLogin} = require('../Validators/loginValidators');
 const { validationResult } = require('express-validator');
 
-
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Lista usuarios filtrados por UID
+ *     tags: [Users]
+ *     parameters:
+ *       - in: query
+ *         name: uid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID del usuario
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios filtrados
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   uid:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ *       404:
+ *         description: Usuario no encontrado
+ *       500:
+ *         description: Error del servidor
+ */
 routerAuth.post('/register', validateCreateUser, (req, res, next) => {
  
   const errors = validationResult(req);
---
+++ Config/Swagger.js
@@ -0,0 +1,15 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'AlfaByte',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./Auth/Router/*.js'], 
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+module.exports = swaggerSpec;
