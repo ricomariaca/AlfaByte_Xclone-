0748dd0071ffea5762f0d22906c34e53703c50a8
Anthony Arango <anthonyarango01@gmail.com>
2024-11-03 22:44:26 -0500

    Conexion chat live

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
-
 require('dotenv').config();
 const express = require("express");
 const { dbConnection } = require('./Databases/config');
 const swaggerUI = require('swagger-ui-express');
 const swaggerSpec = require('./Config/Swagger.js');
+ 
 const cors = require('cors');
-
-const { APP_PORT } = process.env
-
+const { APP_PORT } = process.env;
+const { Server, Socket } = require("socket.io");
+ 
 const app = express();
+const http = require("http");
+const webServer = http.createServer(app);
+ 
+
+const io = new Server(webServer, {
+    connectionStateRecovery: {},
+    cors: {
+      origin: "http://localhost:5173",
+    },
+  });
+
+
+// Middleware
 app.use(cors());
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+ 
+// Base de datos
 dbConnection();
-
+ 
+// Rutas
 const routerPost = require("./Tweet/Router/apiRouter");
 const routerAuth = require("./Auth/Router/apiRouter");
 const routerFollow = require("./Follow/Router/apiRouter");
-
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
+ 
 app.use('/api', routerPost);
 app.use('/api', routerAuth);
 app.use('/api', routerFollow);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+ 
+// Manejo de eventos de socket.io
+io.on('connection', (socket= Socket) => {
+  console.log('A user connected');
 
 
-app.listen(APP_PORT, () => {
-    console.log(`[INFO] SERVER RUNNING A ${APP_PORT}`);
+  socket.on("disconnect", () => {
+    console.info("User disconnected");
+  });
 
+  socket.on("chat message", (payload) => {
+    console.log(`message:  ${JSON.stringify(payload)}`);
+    io.emit("chat message", payload);
+  });
 });
-//77
+
+// Iniciar el servidor en el puerto especificado
+webServer.listen(APP_PORT || 4000, () => {
+    console.log("My server running at port: " + (APP_PORT || 4000));
+  });
+   
+
